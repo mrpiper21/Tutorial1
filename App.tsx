5fc483214ecc2d8de3894d206595bc8999beb0df
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2024-02-07 15:27:12 +0000

    My first react native commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
 
+console.log(Platform.OS)
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: "white", padding:10, marginTop: 20}}>
+       <View>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>Welcome to America</Text>
+      </View>
+      <View style={{ padding: 10, backgroundColor: 'green' }}>
+        <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', alignContent: 'center'}}>
+          <Text style={{color: 'white', fontSize: 40, fontWeight: 'bold'}}>Hello world</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+   
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: 'blue'
+  }
 });
